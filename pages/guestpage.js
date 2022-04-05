0d823d7d186ffca1b32f3e068e56f6c74cd26c88
Thuy Linh Storck <thuylinh90@outlook.de>
2022-04-05 20:49:00 +0200

    changed some style, added to do list function

--- pages/guestpage.js
+++ pages/guestpage.js
@@ -15,7 +15,6 @@ export default function GuestPage() {
           </BigFontStyle>
           <Line></Line>
           <PlusButton />
-
           <InputFormGuest />
         </TextWrapperStyle>
       </MainWrapper>
